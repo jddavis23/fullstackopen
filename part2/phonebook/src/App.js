86b4aeb859d487f01bb48b443de25a094573140b
Jefferson Davis <davisjd.jefferson23@gmail.com>
2023-04-03 16:55:23 +0300

    implimenting search feature

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,16 +3,21 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas'}
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchName, setSearchName] = useState('')
+  const [searchList, setSearchList] = useState([])
 
   const ShallowEqual = () => {
-    console.log(persons.length)
+    //console.log(persons.length)
     for (let i = 0; i < persons.length; i++) {
       if (newName === persons[i].name) {
-        return true;
+        return persons[i]
       }
     }
     return false;
@@ -25,31 +30,51 @@ const App = () => {
       alert(newName + ' is already added to phonebook')
       return
     }
-    const noteObject = {
+    const personObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length + 1
     }
-    setPersons(persons.concat(noteObject))
+    setPersons(persons.concat(personObject))
     setNewName('')
     setNewNumber('')
   }
  
   const handleNameChange = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewName(event.target.value)
   }
 
   const handleNumberAdd = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewNumber(event.target.value)
   }
 
-  
+  const handleSearch = (event) => {
+    //console.log(event.target.value)
+    setSearchName(event.target.value)
+    const reducer = persons.filter((el) => el.name.toLowerCase().includes(searchName.toLowerCase()))
+    console.log(reducer)
+    setSearchList(reducer)
+    // return(
+    //   {personsToShow.map(person =>
+    //   <li key={person.name}>
+    //     {person.name} {person.number}
+    //   </li>
+    // )}
+  }
+
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={searchName} onChange={handleSearch}/>
+        </div>
+      </form>
       <form onSubmit={addName}>
+        <h2>Add new</h2>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
@@ -61,7 +86,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person =>
+      {searchList.map(person =>
         <li key={person.name}>
           {person.name} {person.number}
         </li>
