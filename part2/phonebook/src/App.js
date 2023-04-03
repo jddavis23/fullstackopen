68c73810af93739fcf61db362b8129c4068475ce
Jefferson Davis <davisjd.jefferson23@gmail.com>
2023-04-03 13:27:07 +0300

    completed duplication check

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,60 @@
 import { useState } from 'react'
 
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas'}
   ]) 
   const [newName, setNewName] = useState('')
+
+  const ShallowEqual = () => {
+    console.log(persons.length)
+    for (let i = 0; i < persons.length; i++) {
+      if (newName === persons[i].name) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    if (ShallowEqual())
+    {
+      alert(newName + ' is already added to phonebook')
+      return
+    }
+    const noteObject = {
+      name: newName
+    }
+    setPersons(persons.concat(noteObject))
+    setNewName('')
+  }
+ 
+  const handleNameChange = (event) => {
+    //console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person =>
+        <li key={person.name}>
+          {person.name}
+        </li>
+      )}
     </div>
   )
 }
