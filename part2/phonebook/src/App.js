d64548f3627e21667f7753f55a725cc0272a2ae3
Jefferson Davis <davisjd.jefferson23@gmail.com>
2023-04-04 13:11:40 +0300

    added db.json and included persons object. successfully grabbing data from database and represented in application

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,25 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import Search from './components/Search'
 import Save from './components/Save'
 import Display from './components/Display'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchName, setSearchName] = useState('')
   const [searchList, setSearchList] = useState([])
+
+  useEffect(() => {
+    console.log('we in')
+    axios
+    .get("http://localhost:3001/persons")
+    .then(response => {
+      console.log('successful')
+      setPersons(response.data)
+    })},[])
+    console.log('rendered', persons.length, 'length')
 
   const ShallowEqual = () => {
     //console.log(persons.length)
